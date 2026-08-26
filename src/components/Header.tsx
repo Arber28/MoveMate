ddad825388297638680d6bd7846c268f4e73fa73
Arber <82686423+Arber28@users.noreply.github.com>
2026-08-26 14:06:14 +0000

    v1.0.9 Sticky header geändert

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-zinc-950">
-      <div className="px-5 pt-5 pb-3">
+    <header
+      className="sticky top-0 z-50 bg-zinc-950"
+      style={{
+        paddingTop: "env(safe-area-inset-top)",
+      }}
+    >
+      <div className="px-5 py-3">
         <h1 className="text-3xl font-bold text-center text-white">
           MoveMate
         </h1>
